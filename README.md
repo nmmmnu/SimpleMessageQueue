99566925702124ceacf2c48cabb4011050c0de57
Nikolay Mihaylov <nmmm@xdell5520.(none)>
2013-05-19 19:51:04 +0300

    Rewriten with SimpleLock class

--- README.md
+++ README.md
@@ -2,3 +2,48 @@ SimpleMessageQueue
 ==================
 
 Simple disk based message queue
+
+With this project we want build message queue and to achieve following:
+
+- Messages will be strings. If you need to store something different, is up to you to serialize it.
+- A message must be delivered only once and must *NOT* be lost.
+- Message can be lost only if there are uncorrectable error with data store.
+- Message queue must not get "stuck". This means any errors must be self-correctable.
+- Multiple queues must be supported.
+- Store unique messages only once, in a way similar to Redis sets - sadd() / spop() / sismember()
+- For storage engine we will use memcached. This is because there are lots of compatible services for persistent storage with memcached protocol - memcachedb, tokyocabinet, kyotocabinet, membase etc.
+
+What do not need to be supported
+
+- Messages do not need to be retreived in FIFO order.
+- Server does not need to be scalled.
+
+Misc notes.
+
+The queue works in a well known "Snake-Byte" way, there is a pointer to the header and pointer to the tail.
+
+New messages get stored on the head, and old messages are collected from the tail.
+
+However, because we work really concurrent, we can not just put or pop messages. There are few situations when an error can occure and message can be lost.
+
+For example when a message is pop, a tail is increased, then if message is missing, nobody can be sure if this is empty message, or this is running put() request, that still did not saved the message.
+One possible correction is to wait 5 seconds or so and try collect the message again. However this method is very slow, especially on near empty queue.
+
+There are lot more problems that can not be solved easyly using pure memcached functions.
+
+For this reason, this implementation employ a per queue lock. At some moment of time, only one put() or get() can be performed. This is slow method, but there is no way to lost a message.
+
+Main way to set a lock is to set a key, and then to delete it. However, if something happen in between, for example connection to the database get down,
+the queue will stuck for loooong time.
+
+To avoid this, we employ different kind of lock - we set a key that will expire in 5 seconds or so. 
+This makes another problem - when we delete the key, we could delete some other process lock, because our key may be expired already.
+This could be fixed using unique ID such UUID4. However, to solve this, we delete the key, only if less 5 seconds passed from accuiring the lock.
+
+This immediately means we can not use memcachedb or tokyocabinet as backend, because they do not have expire functionality.
+
+
+
+
+
+
--- simplemessagequeue.py
+++ simplemessagequeue.py
@@ -4,161 +4,226 @@ import memcache
 
 from hashlib import md5
 
+import time
+
+#from uuid import uuid4
+
+_debug = False
+
+class SimpleLocker:
+	r    = None
+		
+	def __init__(self, r, key, timeout = 5):
+		self.r       = r
+		
+		self.key     = key
+		
+		self.timeout = timeout
+		self.start   = 0
+		self.quant   = 0.33
+		
+	def debug(self, s):
+		if _debug:
+			print s
+	
+	def accuire(self):
+		self.start = time.time()
+		while True:
+			if self.r.add(self.key, 1, self.timeout) :
+				self.debug("Lock accuired")
+				return True
+				
+			if time.time() - self.start > self.timeout:
+				self.debug("Lock accuire failed")
+				return False
+			
+			time.sleep(self.quant)
+			self.debug("Lock wait")
+
+	def release(self):
+		if time.time() - self.start < self.timeout - self.quant:
+			self.debug("Lock released")
+			self.r.delete(self.key)
+			return
+		
+		self.debug("Lock release failed")
+			
+
+
 class SimpleMessageQueue:
 	r    = None
 	
-	def __init__(self, r):
-		self.r = r
+	def __init__(self, r, name):
+		self.r        = r
+		self.name     = name
+		self.lock     = SimpleLocker(r, "%s:lock" % name)
+		
+		self.key_head = "%s:%s" % (name, "head")
+		self.key_tail = "%s:%s" % (name, "tail")
 
 
 
-	def _atomic_incr(self, key):
-		if self.r.add(key, 1):
-			return str(1)
-
-		previous = self.r.get(key)		
-		if previous is None:
-			previous = 0
-
-		pointer  = self.r.incr(key)
-
-		if pointer is None:
-			# this can't happen, unless disconected
-			return None
-			
-		if long(previous) > long(pointer):
-			# Overflow detected...
-			return None
-
-		return str(pointer)
+	def debug(self, s):
+		if _debug:
+			print s
 
 
-	
-	def _key_uniq(self, name, message):
+
+	def _get_uniq_key(self, message):
 		try:
 			um = unicode(message).encode('utf8')
 		except UnicodeDecodeError:
 			um = message
 			
-		return name + ":uniq:" + md5(um).hexdigest()
+		return self.name + ":uniq:" + md5(um).hexdigest()
+	
+	
+	
+	def _check_pointer(self, pointer, pointer_name = "pointer"):
+		try:
+			pointer = long(pointer)
+		except ValueError:
+			pointer = 0
+		except TypeError:
+			pointer = 0
 		
+		if pointer == 0:
+			self.debug("%s is 0. Unset or overflow detected" % pointer_name)
+			# (remember we are locked)
+			return None
 		
+		return pointer
+	
+	def _incr_pointer(self, key, pointer_name = "pointer"):
+		self.r.add(key, 0)
+		pointer = self.r.incr(key)
 		
-	def ismember(self, name, message):
-		uk = self._key_uniq(name, message)
+		return self._check_pointer(pointer, pointer_name)
+	
+	def _get_pointer(self, key, pointer_name = "pointer"):
+		pointer = self.r.get(key)
+		
+		return self._check_pointer(pointer, pointer_name)
+
+
+
+	def ismember(self, message):
+		uk = self._get_uniq_key(message)
 		
 		if self.r.get(uk) :
 			# Already in the set
 			return True
 
 		return False
-		
-		
 	
-	def put(self, name, message, uniq=True):
-		if uniq:
-			# Check uniq key
-			if self.ismember(name, message) :
-				# Already in the set
+	
+	
+	def _put(self, message):
+		# Incr "Snake-Byte" head
+		head = self._incr_pointer(self.key_head, "head")
+		
+		if head:
+			data_key = "%s:%ld" % (self.name, head)
+			
+			if self.r.set(data_key, message) :
+				self.debug("PUT() completed")
+
+				# uniq key is set regardless
+				uk = self._get_uniq_key(message)
+				self.r.set(uk, 1)
+
 				return True
-		
-		# uniq key is set regardless
-		uk = self._key_uniq(name, message)
-		self.r.set(uk, 1)
-
-		# Get "Snake-Byte" head
-		head = self._atomic_incr(name + ":head")
-		
-		if head is None:
-			return False
-		
-
-
-		k = name + ":" + head
-		if self.r.set(k, message) :
-			return True
 			
-			
+			# Failed, try to decr back
+			self.r.decr(self.key_head)
+		
+		self.debug("PUT() failed")
 		
 		return False
 	
-	
-	
-	def get(self, name):
-		# Get "Snake-Byte" head, we will probably need it.
-		head = self.r.get(name + ":head")
-		# Get "Snake-Byte" tail.
-		tail = self._atomic_incr(name + ":tail")
+	def put(self, message, uniq=True):
+		if uniq:
+			# Check uniq key, it does not need lock
+			if self.ismember(message) :
+				self.debug("Already in the queue")
+				return True
 		
-		if tail is None:
-			return None
+		# Accuire lock
+		if self.lock.accuire() :
+			# Put the message
+			self._put(message)
+			
+			# Release the lock
+			self.lock.release()
+			
+			return True
+
+		return False
+
+
+
+	def _get(self):
+		# Get "Snake-Byte" head.
+		head = self._get_pointer(self.key_head, "head")
 		
-		k = name + ":" + tail
+		if not head:
+			return False
+
+		# Incr "Snake-Byte" tail.
+		tail = self._incr_pointer(self.key_tail, "tail")
 		
-		message = self.r.get(k)
+		if not tail:
+			return False
+			
+		if tail > head:
+			self.debug("Empty queue")
+			
+			# Rollback, remember we are locked
+			# Instead of decr,
+			# we will implictly set the tail to head,
+			# seems more correct.
+			self.r.set(self.key_tail, head)
+			
+			return False
 
+		data_key = "%s:%ld" % (self.name, tail)
+		
+		message = self.r.get(data_key)
 
-
-		# Delete the data, regardless
-		self.r.delete(k)
-
-
-
-		if message is None:
-			# The tail is probably outside the queue.
-
-			if head is not None :
-				if long(tail) > long(head) :
-					self.r.set(name + ":tail", head)
-					
-			# No need to delete any keys, since there 
-
-			return None
-
-
-
-		# Delete uniq
-		uk = self._key_uniq(name, message)
+		# Delete data, regardless
+		self.r.delete(data_key)
+		
+		uk = self._get_uniq_key(message)
 
 		self.r.delete(uk)
-		
-		
 
 		return message
-		
-		
-		
-	# For Redis compatibility and Common sense names
-	def add(self, name, message):
-		return self.put(name, message)
-		
-	def sadd(self, name, message):
-		return self.put(name, message)
-		
-	def pop(self, name):
-		return self.get(name)
-		
-	def spop(self, name):
-		return self.get(name)
 
-	def sismember(self, name, message):
-		return self.ismember(name, message)
+	def get(self):
+		# Accuire lock
+		if self.lock.accuire() :
+			# Get the message
+			message = self._get()
+			
+			# Release the lock
+			self.lock.release()
+			
+			return message
+
+		return None
 
 
 
-	def info(self, name):
-		head = self.r.get(name + ":head")
-		tail = self.r.get(name + ":tail")
+	def info(self):
+		# No need lock here.
+		head = self._get_pointer(self.key_head, "head")
+		tail = self._get_pointer(self.key_tail, "tail")
 		
 		if head is None :
 			head = 0
-			size = 0
 
 		if tail is None :
 			tail = 0
-
-		head = long(head)
-		tail = long(tail)
 
 		size = head - tail
 		
@@ -166,5 +231,56 @@ class SimpleMessageQueue:
 			size = 0
 		
 		return (size, head, tail)
+
+
+
+
+
+	# For Redis compatibility and Common sense names
+	def add(self, message):
+		return self.put(message)
+		
+	def sadd(self, message):
+		return self.put(message)
+		
+	def pop(self):
+		return self.get()
+		
+	def spop(self):
+		return self.get()
+
+	def sismember(self, message):
+		return self.ismember(message)
+
+
+if __name__ == "__main__":
+	mqmc = memcache.Client(["localhost:1980"], debug=0)
+	mq   = SimpleMessageQueue(mqmc, "niki")
+	
+	"""
+	for i in xrange(50000):
+		if i % 10000 == 0 :
+			print "%8d done" % i
+			
+		if not mq.put("msg2 # %08d" % i) :
+			print "error"
+	"""
+	
+	_debug = True
+	
+	print mq.get()
+	
+	"""
+	while True:
+		msg = mq.get()	
+		if msg :
+			print msg
+		else:
+			break
+	"""
+	
+	print mq.info()
+	
+
 	
 
